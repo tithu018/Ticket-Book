25defef44fdc64929aa40f2ac43ac43817f31cee
tithu018 <e21413@eng.pdn.ac.lk>
2025-10-11 18:46:08 +0530

    Create the trailers section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import FeaturedSection from "../components/FeaturedSection";
+import TrailersSection from "../components/TrailersSection";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <>
         <HeroSection />
         <FeaturedSection />
+        <TrailersSection />
 
     </>
   )
