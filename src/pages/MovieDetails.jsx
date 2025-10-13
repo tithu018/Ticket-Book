bc71bac8a76a20a20ea5442490f9df485d88a7e7
tithu018 <e21413@eng.pdn.ac.lk>
2025-10-13 10:22:08 +0530

    Craete the favorite cast in movie datails page

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -63,6 +63,20 @@ const MovieDetails =()=> {
       </div>
     </div>
   </div>
+
+  <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+  <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+    <div className="flex items-center gap-4 w-max px-4">
+      {show.movie.casts.slice(0,12).map((cast,index)=>(
+        <div key={index} className="flex flex-col items-center text-center">
+          <img src={cast.profile_path} alt={cast.name} className="rounded-full h-20 md:h-20 aspect-square object-cover"></img>
+          <p className="font-medium text-xs mt-3">{cast.name}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+
+  
 </div>
 
   ) : <div>Loading...</div>
